0ac0779e41c4b5a34f2578af96a10cb342a6cf4b
chamathmadhushan1993 <chamathmadushan1993@gmail.com>
2021-10-08 11:11:28 +0530

    matching add MDQuote related handler

--- match-service/src/main/java/com/ust/match/MatchingService.java
+++ match-service/src/main/java/com/ust/match/MatchingService.java
@@ -4,6 +4,7 @@ import com.ust.groupa.domain.entities.instrument.Instrument;
 import com.ust.match.Instrument.OnInstrumentCreateRequest;
 import com.ust.match.Instrument.OnInstrumentHaltRequest;
 import com.ust.match.Instrument.OnInstrumentResumeRequest;
+import com.ust.match.mdQuote.OnMDRequest;
 import com.ustack.common.Injector;
 import com.ustack.service.ServiceProvider;
 
@@ -14,5 +15,6 @@ public class MatchingService extends ServiceProvider<Instrument> {
         registerCmdHandler(OnInstrumentCreateRequest.class);
         registerCmdHandler(OnInstrumentHaltRequest.class);
         registerCmdHandler(OnInstrumentResumeRequest.class);
+        registerCmdHandler(OnMDRequest.class);
     }
 }
--- match-service/src/main/java/com/ust/match/mdQuote/OnMDRequest.java
+++ match-service/src/main/java/com/ust/match/mdQuote/OnMDRequest.java
@@ -1,7 +1,25 @@
 package com.ust.match.mdQuote;
 
-public class OnMDRequest {
-    //TODO command handler
-    //MDQuoteCreated
-    //MDQuoteUpdated
+import com.ust.groupa.domain.entities.instrument.Instrument;
+import com.ust.groupa.domain.entities.instrument.mdquote.MDQuote;
+import com.ust.groupa.domain.entities.instrument.mdquote.commands.MDRequest;
+import com.ust.groupa.domain.entities.instrument.mdquote.events.MDQuoteCreated;
+import com.ust.groupa.domain.entities.instrument.mdquote.events.MDQuoteUpdated;
+import com.ustack.service.core.CmdContext;
+import com.ustack.service.core.EntityCommandHandler;
+import com.ustack.service.core.response.GenericResponse;
+
+import java.util.Optional;
+
+public class OnMDRequest extends EntityCommandHandler<Instrument, MDRequest, GenericResponse> {
+
+    @Override
+    public GenericResponse execute(CmdContext<Instrument> cmdContext, MDRequest cmd) {
+        Optional<MDQuote> quote = cmdContext.getEntity(MDQuote.class, cmd.getSymbol());
+        if (!quote.isPresent())
+            cmdContext.applyEvent(MDQuote.class, cmd.getSymbol(), new MDQuoteCreated(cmd.getSymbol()));
+        MDQuoteUpdated updated = new MDQuoteUpdated(cmd.getSymbol(),cmd.getNbo(),cmd.getNbb(),cmd.getNbboTime());
+        cmdContext.applyEvent(MDQuote.class, cmd.getSymbol(), updated);
+        return GenericResponse.success();
+    }
 }
