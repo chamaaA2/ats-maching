580230384d124f9a5fbc6c2e6cef9ef74f981367
Uditha <uditha.g@ustocktrade.com>
2021-10-18 15:41:17 +0530

    changes for up the system
    Signed-off-by: Uditha <uditha.g@ustocktrade.com>

---
+++ app/src/main/java/com/ust/app/MatchApp.java
@@ -0,0 +1,9 @@
+package com.ust.app;
+
+import com.ustack.app.ReactorApp;
+
+public class MatchApp extends ReactorApp {
+    public static void main(String[] args) throws Exception {
+        ReactorApp.run(MatchApp.class, args);
+    }
+}
